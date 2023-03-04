4d7255d89504ef492f473f73babfaf832ecb9a7d
Lucas G <lucasg.devmail93@gmail.com>
2023-03-03 21:28:52 -0300

    feat: req-06-OK

---
+++ app/backend/src/database/migrations/20230303225627-create-users.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(
+      'users',
+      {
+        id: {
+          field: 'id',
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        username: {
+          field: 'username',
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        role: {
+          field: 'role',
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        email: {
+          field: 'email',
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        password: {
+          field: 'password',
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+      }
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
---
+++ app/backend/src/database/models/Users.ts
@@ -0,0 +1,37 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: STRING(255),
+  },
+  role: {
+    allowNull: false,
+    type: STRING(255),
+  },
+  email: {
+    allowNull: false,
+    type: STRING(255),
+  },
+  password: {
+    allowNull: false,
+    type: STRING(255),
+  },
+}, {
+  sequelize: db,
+});
