2313cca40602b86e5738f2fd9ea8b76f9b7074a7
Lucas G <lucasg.devmail93@gmail.com>
2023-03-08 20:40:51 -0300

    feat: req10 -> OK

--- app/backend/src/api/controllers/UserController.ts
+++ app/backend/src/api/controllers/UserController.ts
@@ -5,6 +5,7 @@ import IServiceUser from '../interfaces/IServiceUser';
 
 export default class UserController {
   private _service: IServiceUser;
+
   constructor(service: IServiceUser) {
     this._service = service;
   }
@@ -12,7 +13,7 @@ export default class UserController {
   async logIn(req:Request, res: Response) {
     const data = await this._service.logIn(req.body);
     if (data.error) {
-      return res.status(401).json({ message: data.message });
+      return res.status(401).json({ message: 'Invalid email or password' });
     }
     const encrypt = JWTOKEN.encript(data);
     return res.status(200).json({ token: encrypt });
